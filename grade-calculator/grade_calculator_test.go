d09528e82fed6f6e8256bb8137211067cbabe78b
Evan Purutcuoglu <tarnation103@gmail.com>
2025-10-02 23:49:00 -0400

    Update grade_calculator_test.go

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -34,6 +34,38 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("test assign a", 71, Assignment)
+	gradeCalculator.AddGrade("test assign b", 74, Exam)
+	gradeCalculator.AddGrade("test assign c", 76, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("test assign d", 61, Assignment)
+	gradeCalculator.AddGrade("test assign e", 64, Exam)
+	gradeCalculator.AddGrade("test assign f", 67, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
 func TestGetGradeF(t *testing.T) {
 	expected_value := "F"
 
@@ -47,5 +79,25 @@ func TestGetGradeF(t *testing.T) {
 
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGradeTypeString(t *testing.T) {
+	expected_value := "assignment"
+
+	if Assignment.String() != expected_value {
+		t.Errorf("Expected Assignment.String() to return '%s'; got '%s' instead", expected_value, Assignment.String())
+	}
+
+	expected_value = "exam"
+
+	if Exam.String() != expected_value {
+		t.Errorf("Expected Exam.String() to return '%s'; got '%s' instead", expected_value, Exam.String())
+	}
+
+	expected_value = "essay"
+
+	if Essay.String() != expected_value {
+		t.Errorf("Expected Essay.String() to return '%s'; got '%s' instead", expected_value, Essay.String())
 	}
 }
